9a2a718045ce44a5df660c7cc79c99452e3b50b9
Anurekha A B <anurekha.a@Thinkpalm.com>
2019-07-25 18:13:30 +0530

    Row and Column headers added

--- src/main/java/com/test/XrayReport.java
+++ src/main/java/com/test/XrayReport.java
@@ -11,6 +11,7 @@ import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.test.xrayapis.TestExecution;
@@ -31,6 +32,7 @@ public class XrayReport {
 	public void sendReportAsExcel(List<TestExecution> testExecution) throws IOException {
 		System.err.println("EXCEL REPORT");
 		int rowCount = 0;
+		rowCount= createRowColumnHeaders(rowCount);
 
 		for (TestExecution test : testExecution) {
 			Row row = sheet.createRow(rowCount++);
@@ -47,6 +49,22 @@ public class XrayReport {
 		fos.close();
 		mail mail = new mail();
 		mail.mailm(testExecutionKey + " execution Result.xlsx");
+	}
+
+	private int createRowColumnHeaders(int rowCount) {
+		Row rowHeader = sheet.createRow(rowCount++);
+		Cell cellRowHeader = rowHeader.createCell(0);
+		cellRowHeader.setCellValue("Test Execution Result for "+testExecutionKey);
+		sheet.addMergedRegion(new CellRangeAddress(0,0,0,1));
+		Row  columnHeader= sheet.createRow(rowCount++);
+		
+		Cell cellcolumnHeader = columnHeader.createCell(0);
+		cellcolumnHeader.setCellValue("Test case ID");
+		
+		Cell cellcolumn2Header = columnHeader.createCell(1);
+		cellcolumn2Header.setCellValue("Status");
+		return rowCount;
+		
 	}
 
 }
